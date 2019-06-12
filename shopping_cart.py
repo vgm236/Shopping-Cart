04dab1b634b020fa9ad7504e41bb8f5a762d5245
vgm236 <50808841+vgm236@users.noreply.github.com>
2019-06-12 18:52:47 -0400

    final_with_comments

--- shopping_cart.py
+++ shopping_cart.py
@@ -89,34 +89,3 @@ print("TOTAL: " + to_usd(total_price))
 print("---------------------------")
 print("THANKS, SEE YOU AGAIN SOON!")
 print("---------------------------")
-
-#>(shopping-env)  --->> python shopping_cart.py
-#>Please input a product identifier: 1
-#>Please input a product identifier: 8
-#>Please input a product identifier: 6
-#>Please input a product identifier: 8
-#>Please input a product identifier: 8
-#>Please input a product identifier: 16
-#>Please input a product identifier: 12
-#>Please input a product identifier: DONE
-#> ---------------------------------
-#> GREEN FOODS GROCERY
-#> WWW.GREEN-FOODS-GROCERY.COM
-#> ---------------------------------
-#> CHECKOUT AT: 2019-06-06 11:31 AM
-#> ---------------------------------
-#> SELECTED PRODUCTS:
-#>  ... Chocolate Sandwich Cookies ($3.50)
-#>  ... Cut Russet Potatoes Steam N' Mash ($4.25)
-#>  ... Dry Nose Oil ($21.99)
-#>  ... Cut Russet Potatoes Steam N' Mash ($4.25)
-#>  ... Cut Russet Potatoes Steam N' Mash ($4.25)
-#>  ... Mint Chocolate Flavored Syrup ($4.50)
-#>  ... Chocolate Fudge Layer Cake ($18.50)
-#> ---------------------------------
-#> SUBTOTAL: $61.24
-#> TAX: $5.35
-#> TOTAL: $66.59
-#> ---------------------------------
-#> THANKS, SEE YOU AGAIN SOON!
-#> ---------------------------------
